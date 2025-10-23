0568ebfce54c19c4892d8907d39e351e7908e3df
mustafalmazz <mustafagfb19077091@gmail.com>
2025-10-23 17:06:23 +0300

    CategoryController Completed and list page added for categories

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -33,7 +33,53 @@ namespace BookManagementApp.Controllers
             }
             _context.Categories.Add(category);
             _context.SaveChanges();
-            return View();
+            return RedirectToAction("List");
+        }
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var category = _context.Categories.FirstOrDefault(x=>x.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            if(category == null)
+            {
+                return NotFound();
+            }
+            var find = _context.Categories.FirstOrDefault(c => c.Id == category.Id);
+            if (find == null)
+            {
+                return NotFound();
+            }
+            find.Id = category.Id;
+            find.CategoryName = category.CategoryName;
+            _context.SaveChanges();
+            return RedirectToAction("List");
+        }
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var bul = _context.Categories.Find(id);
+            if (bul == null)
+            {
+                return NotFound();
+            }
+            _context.Categories.Remove(bul);
+            _context.SaveChanges();
+            return RedirectToAction("List");
         }
     }
 }
